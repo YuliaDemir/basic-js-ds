01474bff8486edbcc7513ab81cced8f52d2166fc
Yulia  Demir <Nightelfin91@gmail.com>
2025-01-06 21:02:23 +0300

    feat: tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,120 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+	start = null;
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	addAll (node) {
+		if (node) {
+			node.add(node.data);
+			if (node.left) 
+				this.addAll(node.left);
+			if (node.right)
+				this.addAll(node.right);
+		}
+	}
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	findParent (data) {
+		let current = this.start;
+		for (;;) {
+			if (current.data === data) 
+				return current;
+			if (current.data > data) {
+				if (current.left) {
+					current = current.left;
+				}
+				else {
+					return current;
+				}
+			}
+			if (current.data < data) {
+				if (current.right) {
+					current = current.right;
+				}
+				else {
+					return current;
+				}
+			}
+		}
+	}
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+	root() {
+	  return this.start;
+	}
+  
+	add(data) {
+		const newNode = new Node(data);
+		if (!this.start) {
+			start = newNode;
+		}
+		else {
+			const parent = findParent(data);
+			if (parent.data > data) {
+				parent.left = newNode;
+			}
+			else if (parent.data < data) {
+				parent.right = newNode;
+			}
+		}
+	}
+  
+	has(data) {
+		return findParent(data).data === data ? true : false;
+	}
+  
+	find(data) {
+		const parent = findParent(data);
+		if (parent.data === data) 
+			return parent;
+		return null;
+	}
+  
+	remove(data) {
+		let currentParent = this.start;
+		for (;;) {
+			if (current.data === data) 
+				curent;
+			if (currentParent.data > data) {
+				if (current.left) {
+					current = current.left;
+				}
+				else {
+					return current;
+				}
+			}
+			if (current.data < data) {
+				if (current.right) {
+					current = current.right;
+				}
+				else {
+					return current;
+				}
+			}
+		}
+		const parent = findParent(data);
+		if (parent?.right.data === data) {
+			addAll (parent.left);
+			addAll (parent.right);
+		}		
+	}
+  
+	min() {
+		let current = this.start;
+	  while (current.left) {
+		current = current.left;
+	  }
+	}
+  
+	max() {
+		let current = this.start;
+		while (current.right) {
+		  current = current.right;
+	}
   }
 }
 
